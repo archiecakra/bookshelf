dd93e5242774da27d613b4f2fe20ca3ba10d5f66
titit <archiecakra1@gmail.com>
2021-07-23 21:08:01 +0700

    adding some

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,25 @@
-const bookShelf = require('./bookshelf');
+// const bookShelf = require('./bookshelf');
+const {nanoid} = require('nanoid');
 
 const addBookHandler = (request, h) => {
+  // const {
+  // name,
+  // year,
+  // author,
+  // summary,
+  // publisher,
+  // pageCount,
+  // readPage,
+  // reading,
+  // } = request.payload;
 
+  // console.log(request.payload);
+  return request.payload;
+
+  const id = nanoid(8);
+  // const finished = readPage === pageCount ? true : false;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
 };
 
 module.exports = {addBookHandler};
